c3e4017dfea9d307c0ec3e4b79d7c7041ab54caf
Thim <thim.anneessens@smals.be>
2020-06-28 16:44:39 +0200

    Fix site title and description

--- src/pages/index.js
+++ src/pages/index.js
@@ -77,8 +77,8 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Documentation site for https://www.eboxenterprise.be. Discover how to become a Message Registry, how to integrate with Oauth, the EES or the Federation Service.">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
